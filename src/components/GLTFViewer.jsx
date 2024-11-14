10486decac64b1e22e0f0d1bb77a724379d92a35
KoushikMote <koushik010216@gmail.com>
2024-11-14 16:26:40 -0330

    gltf

--- src/components/GLTFViewer.jsx
+++ src/components/GLTFViewer.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, useGLTF } from '@react-three/drei';
+import modelUrl from './3d_model/scene.gltf';
 
 // Model Component
-const Model = ({ url }) => {
-  const { scene } = useGLTF(url); // Loads the GLTF file
+const Model = () => {
+  const { scene } = useGLTF(modelUrl); 
   return <primitive object={scene} />;
 };
 
@@ -15,7 +16,7 @@ const GLTFViewer = () => {
       <Canvas camera={{ position: [250, 200, 650], fov: 50 }}>
         <ambientLight intensity={0.5} />
         <directionalLight position={[5, 5, 5]} intensity={1} />
-        <Model url="./3d_model/scene.gltf" />
+        <Model url={modelUrl}/>
         <OrbitControls />
       </Canvas>
     </div>
